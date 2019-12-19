3b40fd5090aee47f1f7691f0b6796f0e5a5d63bb
Lion-Kang <53907943+Lion-Kang@users.noreply.github.com>
2019-12-18 16:16:49 -0800

    first commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
-/**
- Tech Degree Project 3
- Name: Brice Amarasinghe 
-**/
+// Select first input field on page load
+$("#name").focus();
 
-console.log('test if html is linked');
+// Hide "Your Job Role" text input
+$("#other-title").hide();
+
+// if "other" is selected, show text input box
+$("#title").change(function(){
+    if($("#title option:selected").val() == "other"){
+        $("#other-title").show();
+    } else {
+        $("#other-title").hide();
+    }
+});
+
+// Hide "Select Theme" option from drop menu
+$("#design").children().first().remove();
+
+// Prepend instruction to beginning of 'color' select tag
+$("#color").prepend("<option>Please select a T-Shirt Theme</option>");
+
+// Set default select value to new prepended option
+$("#color").val('Please select a T-Shirt Theme');
+
+// show colors for only "Theme - JS Puns"
+$("#design").change(function(){
+    if($("#design option:selected").val() == "js puns"){
+        $("#colors-js-puns").show();
+    } else {
+        $("#colors-js-puns").hide();
+    }
+}); 
